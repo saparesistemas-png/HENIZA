528dd4458e5c0cf65ec537135a2b4a7a03e85f0f
saparesistemas-png <sapare.sistemas@gmail.com>
2026-09-15 22:31:19 -0300

    feat: ensureStorage no boot (Dexie ou SQLite)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ensureStorage } from './storage';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function boot() {
+  try {
+    await ensureStorage();
+  } catch (e) {
+    console.warn('[HENIZA] storage boot', e);
+  }
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+void boot();
